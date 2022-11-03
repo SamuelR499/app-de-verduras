5201a0f4d91c5d5a43d2700a5e376251d6b87162
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-03 13:21:21 -0300

    initial commit react-native project

---
+++ src/telas/Cesta.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Image, StyleSheet, Dimensions, Text, View } from 'react-native';
+
+import topo from '../../assets/topo.png'
+import logo from '../../assets/logo.png'
+
+const width = Dimensions.get('screen').width
+
+export default function Cesta(){
+  return (
+  <>
+    <Image source={topo} style={style.topo} />
+    <Text style={style.titulo}>Detalhe da cesta</Text>
+  
+    <View style={style.cesta}>
+      <Text style={style.nome}>Cesta de Verduras</Text>
+      <View style={style.fazenda}>
+      <Image source={logo} style={style.imagemFazenda}/>
+      <Text style={style.nomeFazenda}>Jenny Jack Farma</Text>
+      </View>
+      <Text style={style.descricao}>
+        Uma cesta com produtos selecionados
+        cuidadosamente da fazenda para
+        sua cozinha
+      </Text>
+      <Text style={style.preco}>R$ 40,00</Text>
+    </View>
+  </>
+  )
+}
+
+const style = StyleSheet.create({
+  topo: {
+    width: "100%",
+    height: 578 / 768 * width
+  },
+  titulo: {
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    fontSize: 16,
+    lineHeight: 26,
+    color: "white",
+    fontWeight: "bold",
+    padding: 16
+  },
+  cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16
+  },
+  nome: {
+    color: "#464646",
+    fontSize: 26,
+    lineHeight: 42,
+    fontFamily: "MontserratBold"
+  },
+  fazenda: {
+    flexDirection: "row",
+    paddingVertical: 12
+  },
+  imagemFazenda: {
+    width: 32,
+    height:32
+  },
+  nomeFazenda: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+    fontFamily: "MontserratRegular"
+  },
+  descricao: {
+    color: "#A3A3A3",
+    fontSize: 16,
+    lineHeight: 26
+  },
+  preco: {
+    color: "#2A9F85",
+    fontWeight: "bold",
+    fontSize: 26,
+    lineHeight: 42,
+    marginTop: 8
+  },
+})
