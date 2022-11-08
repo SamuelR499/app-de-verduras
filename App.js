571aee91430537c38b486579c30e1359fdb85bd7
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-08 11:08:16 -0300

    refactor: import React e refatora erros de escrita

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
-import { StatusBar, SafeAreaView, View } from 'react-native';
-import {
-  useFonts,
-  Montserrat_400Regular,
+import React from 'react';
+import { StatusBar, SafeAreaView } from 'react-native';
+import { 
+  useFonts, 
+  Montserrat_400Regular, 
   Montserrat_700Bold,
 } from '@expo-google-fonts/montserrat';
-import AppLoading from 'react-native'
+import AppLoading from 'expo-app-loading';
+
 import Cesta from './src/telas/Cesta';
 import mock from './src/mocks/cesta';
 
 export default function App() {
-  const [fontCarregada] = useFonts({
-    "MontSerratRegular": Montserrat_400Regular,
-    "MontSerratBold": Montserrat_700Bold,
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
   });
 
-  if (!fontCarregada) {
+  if (!fonteCarregada) {
     return <AppLoading />
   }
 
   return (
     <SafeAreaView>
-      <StatusBar/>
+      <StatusBar />
       <Cesta {...mock} />
     </SafeAreaView>
   );
