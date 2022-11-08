37a70453adae518be0038c20b598cf961f326999
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-08 10:16:57 -0300

    adicionando AppLoading

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {
   Montserrat_400Regular,
   Montserrat_700Bold,
 } from '@expo-google-fonts/montserrat';
+import AppLoading from 'react-native'
 import Cesta from './src/telas/Cesta';
 import mock from './src/mocks/cesta';
 
@@ -14,7 +15,7 @@ export default function App() {
   });
 
   if (!fontCarregada) {
-    return <View />
+    return <AppLoading />
   }
 
   return (
